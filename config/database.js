958f6d403422a0e3b71615c68254590de5093fe2
stepanlihoman66-hub <stepanlihoman66@gmail.com>
2026-04-01 18:27:06 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,16 +1,18 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: 'postgres',
-        logging: false
+// Определяем, где мы находимся: на Render или локально
+const isProduction = process.env.NODE_ENV === 'production';
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: isProduction ? {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
-);
+  } : {},
+  logging: false
+});
 
 module.exports = sequelize;
